6b444a9a48a9582b4ae2c85b3a837cd33a5474f1
Edward Omopariola <femiedjr@gmail.com>
2025-03-15 08:10:32 -0400

    creating a media app using react redux and other library included

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import { StrictMode } from "react";
+import './index.css';
+import React from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.js";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store }from "./store";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const el = document.getElementById("root");
+const root = createRoot(el);
+
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
+
